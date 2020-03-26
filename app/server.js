66c1c7e93f0512b279a60b8b98884544d78ea4c1
Pradeep Joshi <pradeepjoshi@Pradeeps-MBP.attlocal.net>
2020-03-26 13:38:37 -0700

    Fixed attributes

--- app/server.js
+++ app/server.js
@@ -7,7 +7,7 @@ const HOST = '0.0.0.0';
 
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Welcome to Azure AKS Demo Page !!.. \n');
+  res.send('<h1>Welcome to Azure AKS Demo Page !!..</h1> \n');
 });
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
